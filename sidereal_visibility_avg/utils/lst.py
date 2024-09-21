6c57c12af8a2f394a4541ac24462abe1e469e940
jurjen93 <jurjendejong93@gmail.com>
2024-09-21 11:17:21 +0200

    faster mjd

--- sidereal_visibility_avg/utils/lst.py
+++ sidereal_visibility_avg/utils/lst.py
@@ -1,20 +1,15 @@
+import numpy as np
 from astropy.time import Time
 from astropy.coordinates import EarthLocation
 import astropy.units as u
+from joblib import Parallel, delayed
+from multiprocessing import cpu_count
 
 
-def mjd_seconds_to_lst_seconds(mjd_seconds, longitude_deg=6.869837):
+def mjd_seconds_to_lst_seconds_single(mjd_seconds, longitude_deg=6.869837):
     """
-    Convert time in modified Julian Date time to LST.
-
-    :param:
-        - mjd_seconds: modified Julian date time in seconds
-        - longitde_deg: longitude telescope in degrees (6.869837 for LOFAR core)
-
-    :return:
-        time in LST
+    Convert time in modified Julian Date time to LST for a single value or small chunk.
     """
-
     # Convert seconds to days for MJD
     mjd_days = mjd_seconds / 86400.0
 
@@ -31,3 +26,28 @@ def mjd_seconds_to_lst_seconds(mjd_seconds, longitude_deg=6.869837):
     lst_seconds = lst_hours * 3600.0
 
     return lst_seconds
+
+
+def mjd_seconds_to_lst_seconds(mjd_seconds, longitude_deg=6.869837):
+    """
+    Convert time in modified Julian Date time to LST with parallel processing.
+
+    :param:
+        - mjd_seconds: Array of modified Julian date time in seconds
+        - longitude_deg: Longitude telescope in degrees (default: 6.869837 for LOFAR core)
+
+    :return:
+        - Array of time in LST (seconds)
+    """
+    # Split the mjd_seconds array into chunks for parallel processing
+    n_jobs = max(cpu_count() - 1, 1)  # Use all available cores minus 1
+    chunk_size = len(mjd_seconds) // n_jobs
+
+    # Perform parallel processing
+    lst_seconds = Parallel(n_jobs=n_jobs)(
+        delayed(mjd_seconds_to_lst_seconds_single)(mjd_seconds[i:i + chunk_size], longitude_deg)
+        for i in range(0, len(mjd_seconds), chunk_size)
+    )
+
+    # Concatenate results from different workers
+    return np.concatenate(lst_seconds)
