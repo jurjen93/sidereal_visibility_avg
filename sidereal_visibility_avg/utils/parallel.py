ecd35c6b444fc5100394b1d4a7edd2c0bc1dfb97
jurjen93 <jurjendejong93@gmail.com>
2024-11-08 20:38:33 +0100

    refactor

--- sidereal_visibility_avg/utils/parallel.py
+++ sidereal_visibility_avg/utils/parallel.py
@@ -44,9 +44,7 @@ def sum_arrays_chunkwise(array1, array2, chunk_size=1000, n_jobs=-1, un_memmap=T
 
     n = len(array1)
 
-    # Determine the output storage type based on input type
     if isinstance(array1, np.memmap) or isinstance(array2, np.memmap):
-        # Create a temporary file to store the result as a memmap
         temp_file = tempfile.NamedTemporaryFile(delete=False)
         result_array = np.memmap(temp_file.name, dtype=array1.dtype, mode='w+', shape=array1.shape)
     else:
@@ -55,10 +53,8 @@ def sum_arrays_chunkwise(array1, array2, chunk_size=1000, n_jobs=-1, un_memmap=T
     def sum_chunk_to_result(start, end):
         result_array[start:end] = array1[start:end] + array2[start:end]
 
-    # Create a generator for chunk indices
     chunks = ((i, min(i + chunk_size, n)) for i in range(0, n, chunk_size))
 
-    # Parallel processing with threading preferred for better I/O handling
     Parallel(n_jobs=n_jobs, prefer="threads")(delayed(sum_chunk_to_result)(start, end) for start, end in chunks)
 
     return result_array
