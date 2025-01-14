5f254eafc65dafa51c65a2801322c5da9c6af2dd
jurjen93 <jurjendejong93@gmail.com>
2025-01-14 17:12:19 +0100

    plotting

--- sidereal_visibility_avg/plotting.py
+++ sidereal_visibility_avg/plotting.py
@@ -1,9 +1,12 @@
 from sys import exit
+from os import system as run_command
+from argparse import ArgumentParser
+
+import numpy as np
 import matplotlib.pyplot as plt
 from casacore.tables import table
-from os import system as run_command
-import numpy as np
-from .ms_info import make_ant_pairs, get_station_id
+
+from .utils.ms_info import make_ant_pairs, get_station_id
 
 
 def plot_baseline_track(t_final_name: str = None, t_input_names: list = None, baseline='0-1', UV=True, saveas=None):
@@ -39,7 +42,7 @@ def plot_baseline_track(t_final_name: str = None, t_input_names: list = None, ba
         print(ref_stats[int(float(ant1))], ref_stats[int(float(ant2))])
 
         with table(t_final_name, ack=False) as f:
-            fsub = f.query(f'ANTENNA1={ant1} AND ANTENNA2,={ant2} AND NOT ALL(WEIGHT_SPECTRUM == 0)', columns='UVW')
+            fsub = f.query(f'ANTENNA1={ant1} AND ANTENNA2={ant2} AND NOT ALL(WEIGHT_SPECTRUM == 0)', columns='UVW')
             uvw1 = fsub.getcol("UVW")
 
         with table(t_input_name, ack=False) as f:
@@ -93,3 +96,26 @@ def make_baseline_uvw_plots(tabl, mslist):
     for baseline in baselines:
         bl = '-'.join([str(a) for a in baseline])
         plot_baseline_track(tabl, sorted(mslist), bl, saveas=f'baseline_plots/{bl}.png')
+
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+
+    parser = ArgumentParser(description='Plot UV tracks for baseline')
+    parser.add_argument('msin', nargs='+', help='Measurement sets to combine (up to 4).')
+    parser.add_argument('--msout', type=str, default='empty.ms', help='Measurement set output name.')
+    parser.add_argument('--baseline', type=str, default='0-1', help='Baseline numbers')
+    parser.add_argument('--saveas', type=str, default=None, help='Name of PNG (if not given, it just plots)')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    plot_baseline_track(t_final_name=args.msout, t_input_names=args.msin, baseline=args.baseline, saveas=args.saveas)
+
+
+if __name__ == '__main__':
+    main()
